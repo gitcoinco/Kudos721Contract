faad7daec86ba0f895d8188fc9a9289b863698ac
Jason Haas <jasonrhaas@gmail.com>
2018-06-12 22:01:28 -0500

    Add all the Kudos artifacts for testing

--- erc721/test/test_minting.py
+++ erc721/test/test_minting.py
@@ -9,8 +9,62 @@ checksummed = Web3.toChecksumAddress('0xf41d6039b2af11c40cd02d835bf9e464e148007b
 
 kudos = w3.eth.contract(address=checksummed, abi=abi)
 
-kudos.functions.create('pythonista', 'good at the pythons', 1234, 500000, 3).transact({'from': w3.eth.coinbase})
+bugsquasher = dict(name='Bugsquasher',
+                   description='exterminator',
+                   rareness=1234,
+                   price=50000,
+                   numClonesAllowed=3
+                   )
+
+collabmachine = dict(name='Collaborationmachine',
+                     description='plays nice with others',
+                     rareness=1234,
+                     price=50000,
+                     numClonesAllowed=3
+                     )
+
+designstar = dict(name='Designstar',
+                  description='makes nice things',
+                  rareness=1234,
+                  price=50000,
+                  numClonesAllowed=3
+                  )
+
+fastturn = dict(name='Fastturnaround',
+                description='speedy gonzalez',
+                rareness=1234,
+                price=50000,
+                numClonesAllowed=3
+                )
+
+helpinghand = dict(name='Helpinghand',
+                   description='like a good neighbor',
+                   rareness=1234,
+                   price=50000,
+                   numClonesAllowed=3
+                   )
+
+problemsolver = dict(name='Problemsolver',
+                     description='exterminator',
+                     rareness=1234,
+                     price=50000,
+                     numClonesAllowed=3
+                     )
+
+pythonista = dict(name='Pythonista',
+                  description='follows the zen of python',
+                  rareness=1234,
+                  price=50000,
+                  numClonesAllowed=3
+                  )
+
+artifacts = (bugsquasher, collabmachine, designstar, fastturn, helpinghand, problemsolver, pythonista)
+
+for artifact in artifacts:
+    try:
+        kudos.functions.create(**artifact).transact({'from': w3.eth.coinbase})
+    except ValueError as e:
+        print(f'{artifact["name"]} artifact already exists.  Skipping creation.')
 
 
 print(kudos.functions.totalSupply().call())
-print(kudos.functions.getTokenName(1).call())
