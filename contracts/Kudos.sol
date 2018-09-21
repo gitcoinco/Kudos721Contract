ca23fc725f54f00b535a4e0b8052dce231c8ca87
Jason Haas <jasonrhaas@gmail.com>
2018-09-21 16:28:08 -0500

    Add documentation to Kudos.sol contract

--- contracts/Kudos.sol
+++ contracts/Kudos.sol
@@ -3,28 +3,35 @@ pragma solidity ^0.4.24;
 import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
+/// @title Kudos
+/// @author Jason Haas <jasonrhaas@gmail.com>
+/// @notice Kudos ERC721 interface for minting, cloning, and transferring Kudos tokens.
 contract Kudos is ERC721Token("KudosToken", "KDO"), Ownable { 
     struct Kudo {
         string name;
-        string description;         // move to metadata in IPFS?
+        string description;
         uint256 rarity;
-        uint256 priceFinney;              // price in finney
+        uint256 priceFinney;
         uint256 numClonesAllowed;
         uint256 numClonesInWild;
-        string tags;                // move to metadata in IPFS?
-        string image;               // IPFS Hash
-        uint256 clonedFromId;       // id of gen0 kudos
+        string tags;
+        string image;
+        uint256 clonedFromId;
     }
 
     Kudo[] public kudos;
 
-    // constructor (Kudo[] kudos) public {
-    //     // Throw away the first index, and use 0 to check if the data exists
-    //     kudos.push(0);
-    // }
-
     mapping(string => uint256) internal nameToTokenId;
 
+    /// @dev mint(): Mint a new Gen0 Kudos.  These are the tokens that other Kudos will be "cloned from".
+    /// @param name The name of the new Kudos to mint.  Should be lowercase_with_underscores.
+    /// @param description Description of the Kudos.
+    /// @param rarity Rarity score of the Kudos, from 0 to 100.  May be deprecated in the future.
+    /// @param priceFinney Price of the Kudos in Finney.
+    /// @param numClonesAllowed Maximum number of times this Kudos is allowed to be cloned.
+    /// @param tags Comma delimited tags for the Kudos.
+    /// @param image Image file name.  Such as pythonista.svg.
+    /// @return the tokenId of the Kudos that has been minted.  Note that in a transaction only the tx_hash is returned.
     function mint(string name, string description, uint256 rarity, uint256 priceFinney, uint256 numClonesAllowed, string tags, string image) public payable onlyOwner returns (uint256 tokenId) {
         // Ensure that each Gen0 Kudos is unique
         require(nameToTokenId[name] == 0);
@@ -53,9 +60,11 @@ contract Kudos is ERC721Token("KudosToken", "KDO"), Ownable {
         _mint(msg.sender, tokenId);
 
         nameToTokenId[name] = tokenId;
-        // return tokenId;
     }
 
+    /// @dev clone(): Clone a new Kudos from a Gen0 Kudos.
+    /// @param name The name of the new Kudos to mint.  Should be lowercase_with_underscores.
+    /// @param numClonesRequested Number of clones to generate.
     function clone(string name, uint256 numClonesRequested) public payable {
         // Grab existing Kudo blueprint
         uint256 gen0KudosId = nameToTokenId[name];
@@ -92,9 +101,12 @@ contract Kudos is ERC721Token("KudosToken", "KDO"), Ownable {
             // Mint the new kudos to the msg.sender's account
             _mint(msg.sender, tokenId);
         }
-
     }
 
+    /// @dev cloneAndTransfer(): Clone a new Kudos and then transfer it to a different address.
+    /// @param name The name of the new Kudos to mint.  Should be lowercase_with_underscores.
+    /// @param numClonesRequested Number of clones to generate.
+    /// @param receiver The address to transfer the Kudos to after it's cloned.
     function cloneAndTransfer(string name, uint256 numClonesRequested, address receiver) public payable {
         // Grab existing Kudo blueprint
         uint256 gen0KudosId = nameToTokenId[name];
@@ -130,17 +142,23 @@ contract Kudos is ERC721Token("KudosToken", "KDO"), Ownable {
             uint256 tokenId = kudos.push(_newKudo) - 1;
 
             _mint(receiver, tokenId);
-            // transferFrom(msg.sender, receiver, tokenId);
         }
-
     }
 
+    /// @dev burnGen0(): Burn a Gen0 Kudos token.  Can only be performed by the owner of the token.
+    /// @param owner The owner address of the token to burn.
+    /// TODO:  Do we need to pay the owner or can we use the ``ownerOf()` function?
+    /// @param tokenId The Kudos ID to be burned.
     function burnGen0(address owner, uint256 tokenId) public payable onlyOwner {
         Kudo memory _kudo = kudos[tokenId];
         delete nameToTokenId[_kudo.name];
         _burn(owner, tokenId);
     }
 
+    /// @dev burn(): Burn Kudos token.
+    /// @param owner The owner address of the token to burn.
+    /// TODO:  Do we need to pay the owner or can we use the ``ownerOf()` function?
+    /// @param tokenId The Kudos ID to be burned.
     function burn(address owner, uint256 tokenId) public payable {
         Kudo memory _kudo = kudos[tokenId];
         uint256 gen0Id = nameToTokenId[_kudo.name];
@@ -150,7 +168,9 @@ contract Kudos is ERC721Token("KudosToken", "KDO"), Ownable {
         _burn(owner, tokenId);
     }
 
-
+    /// @dev getKudosById(): Return a Kudos struct/array given a Kudos Id. 
+    /// @param tokenId The Kudos Id.
+    /// @return the Kudos struct, in array form.
     function getKudosById(uint256 tokenId) view public returns (string name, string description, uint256 rarity,
                                                                 uint256 priceFinney, uint256 numClonesAllowed, uint256 numClonesInWild,
                                                                 string tags, string image, uint256 clonedFromId)
@@ -168,12 +188,11 @@ contract Kudos is ERC721Token("KudosToken", "KDO"), Ownable {
         clonedFromId = _kudo.clonedFromId;
     }
 
-
+    /// @dev getGen0TokenId(): Get the Gen0 Kudos Id.
+    /// @param name The name Gen0 Kudos.
+    /// @return Kudos Id.
     function getGen0TokenId(string name) view public returns (uint256) {
         // If the result is 0 it means the key was not found in the mapping.
-        // string memory name = nameToTokenId[name];
-        // if(name == 0) throw;
         return nameToTokenId[name];
     }
-
 }
