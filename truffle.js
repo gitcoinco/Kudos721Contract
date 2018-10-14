a5567d97a91a8cadb5a180d709fe4a3ea7bae4eb
Jason Haas <jasonrhaas@gmail.com>
2018-10-14 18:35:17 -0400

    Add privKeys support for mainnet deploy

--- truffle.js
+++ truffle.js
@@ -1,25 +1,33 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
+const HDWalletProviderPriv = require('truffle-hdwallet-provider-privkey')
 const fs = require('fs')
 
 // First read in the secrets.json to get our mnemonic
 let secrets
 let mnemonic
+let privkeys
 
 if (fs.existsSync('secrets.json')) {
   secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'))
   mnemonic = secrets.mnemonic
   token = secrets.token
+  privkeys = secrets.privkeys
 } else {
   console.log('No secrets.json found. If you are trying to publish EPM ' +
               'this will fail. Otherwise, you can ignore this message!')
   mnemonic = ''
   token = ''
+  privkeys = []
 }
 
 module.exports = {
   networks: {
     live: {
-      network_id: 1 // Ethereum public network
+      provider: function() {
+        return new HDWalletProviderPriv(privkeys, 'https://mainnet.infura.io/' + token)
+      },
+      network_id: 1, // Ethereum public network
+      from: '0x6239FF1040E412491557a7a02b2CBcC5aE85dc8F',
       // optional config values
       // host - defaults to "localhost"
       // port - defaults to 8545
